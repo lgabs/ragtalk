8ae625af16c7a51d5e9e3c24252052ed9bea3a55
Luan Fernandes <luan.fernandes@buser.com.br>
2024-03-25 15:54:56 -0300

    add make embedding function

--- src/ragtalk/settings.py
+++ src/ragtalk/settings.py
@@ -1,16 +1,17 @@
 import tomllib
 from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn, FilePath, SecretStr
+from pydantic import PostgresDsn, FilePath, SecretStr, Field
 
 
 class Settings(BaseSettings):
     environment: str
     database_url: PostgresDsn
-    params_path: FilePath
+    params_path: FilePath = Field(default="/data/params.toml")
     openai_api_key: SecretStr
+    knowledge_base_path: FilePath = Field(default="./data/knowledge_base.csv")
 
 
-config = Settings.model_validate({})
+config = Settings()
 
 with open(config.params_path, "rb") as file:
     chain_params = tomllib.load(file)
