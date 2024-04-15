be58bbb50667f7c631c2a54ceb481ef066e3f698
Luan Fernandes <luan.fernandes@buser.com.br>
2024-04-15 17:45:01 -0300

    update chain

--- src/ragtalk/chains/rag_with_history.py
+++ src/ragtalk/chains/rag_with_history.py
@@ -1,6 +1,6 @@
 from operator import itemgetter
 
-from typing import TypedDict, List, Tuple
+from typing import TypedDict
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts.prompt import PromptTemplate
@@ -12,17 +12,18 @@ from langchain.schema.runnable import RunnableMap, RunnablePassthrough
 
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-from ragtalk.settings import chain_params
+from ragtalk.settings import chain_settings
 from ragtalk.memory import get_message_history, format_chat_history
 from ragtalk.retriever import get_retriever, combine_documents
 
-STANDALONE_QUESTION_TEMPLATE = chain_params.get("prompts").get(
-    "standalone_question_template"
-)
+
+# Prompts
+prompts = chain_settings.chain_params.get("prompts")
+STANDALONE_QUESTION_TEMPLATE = prompts.get("standalone_question_template")
 STANDALONE_QUESTION_PROMPT = PromptTemplate.from_template(STANDALONE_QUESTION_TEMPLATE)
 
-RAG_ANSWER_TEMPLATE = chain_params.get("prompts").get("rag_answer_template")
-RAG_ANSWER_PROMPT = ChatPromptTemplate.from_template(RAG_ANSWER_TEMPLATE)
+FINAL_ANSWER_TEMPLATE = prompts.get("final_answer_template")
+FINAL_ANSWER_PROMPT = ChatPromptTemplate.from_template(FINAL_ANSWER_TEMPLATE)
 
 _inputs = RunnableMap(
     standalone_question=RunnablePassthrough.assign(
@@ -32,7 +33,17 @@ _inputs = RunnableMap(
     | ChatOpenAI(temperature=0)
     | StrOutputParser(),
 )
+
+# LLM (for main task)
+MODEL_PARAMS = chain_settings.chain_params.get("model_params", {})
+llm = ChatOpenAI(
+    **MODEL_PARAMS, openai_api_key=chain_settings.openai_api_key.get_secret_value()
+)
+
+# Retriever
 retriever = get_retriever()
+
+# Build the chain
 _context = {
     "context": itemgetter("standalone_question") | retriever | combine_documents,
     "question": lambda x: x["standalone_question"],
@@ -40,7 +51,7 @@ _context = {
 
 
 conversational_qa_chain = (
-    _inputs | _context | RAG_ANSWER_PROMPT | ChatOpenAI() | StrOutputParser()
+    _inputs | _context | FINAL_ANSWER_PROMPT | llm | StrOutputParser()
 )
 
 
